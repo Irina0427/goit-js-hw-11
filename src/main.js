9ddbc5e8860e3c333eb16d33e07232ae6d9c0c6b
Irina <yaychenya2014@gmail.com>
2025-09-15 22:06:07 +0300

    first hm-11 main

--- src/main.js
+++ src/main.js
@@ -1,57 +1,49 @@
 
-import '../styles.css';
+
+import './css/styles.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import { getImagesByQuery } from './pixabay-api.js';
-import {
-  createGallery,
-  clearGallery,
-  showLoader,
-  hideLoader,
-} from './render-functions.js';
+import { getImagesByQuery } from './js/pixabay-api.js';
+import { createGallery, clearGallery, showLoader, hideLoader } from './js/render-functions.js';
+const form = document.querySelector('.form');
 
-const form = document.getElementById('search-form');
-const input = form.elements['search-text'];
-const submitBtn = form.querySelector('button');
 
-form.addEventListener('submit', async e => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
+  const query = e.currentTarget.elements['search-text'].value.trim();
 
-  const query = input.value.trim();
   if (!query) {
     iziToast.warning({
-      title: 'Oops',
-      message: 'Please type something to search.',
+      message: 'Please enter a search query',
       position: 'topRight',
     });
     return;
   }
+
   clearGallery();
   showLoader();
-  submitBtn.disabled = true;
 
-  try {
-    const data = await getImagesByQuery(query);
+  getImagesByQuery(query)
+    .then((data) => {
+      hideLoader();
 
-    if (!data.hits || data.hits.length === 0) {
-      iziToast.info({
-        title: 'No results',
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
+      if (!data.hits || data.hits.length === 0) {
+        iziToast.info({
+          message: 'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      }
+
+      createGallery(data.hits);
+    })
+    .catch((error) => {
+      hideLoader();
+      iziToast.error({
+        message: 'Request failed. Check API key or network.',
         position: 'topRight',
       });
-      return;
-    }
-    createGallery(data.hits);
-  } catch (err) {
-    iziToast.error({
-      title: 'Error',
-      message: err?.message || 'Something went wrong. Try again later.',
-      position: 'topRight',
+      console.error(error);
     });
-  } finally {
-    hideLoader();
-    submitBtn.disabled = false;
-  }
 });
