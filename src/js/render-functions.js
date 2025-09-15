f12e3f993f876edc6353910e637acfa60b7c99d7
Irina <yaychenya2014@gmail.com>
2025-09-15 23:07:50 +0300

    my first hm-11-1

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,71 +2,48 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const gallery = document.querySelector('.gallery');
-const loader = document.getElementById('loader');
-
+const galleryEl = document.querySelector('.gallery');
+const loaderEl  = document.querySelector('.loader'); 
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
-  captionPosition: 'bottom',
 });
 
-export function createGallery(images) {
+export function createGallery(images = []) {
   const markup = images
     .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `
+      ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
       <li class="gallery-item">
         <a class="gallery-link" href="${largeImageURL}">
-          <img
-            class="gallery-image"
-            src="${webformatURL}"
-            alt="${escapeHtml(tags)}"
-            loading="lazy"
-          />
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
         </a>
-        <div class="card-meta">
-          <div><b>Likes</b>${likes}</div>
-          <div><b>Views</b>${views}</div>
-          <div><b>Comments</b>${comments}</div>
-          <div><b>Downloads</b>${downloads}</div>
-        </div>
+        <ul class="info">
+          <li><b>Likes:</b> ${likes}</li>
+          <li><b>Views:</b> ${views}</li>
+          <li><b>Comments:</b> ${comments}</li>
+          <li><b>Downloads:</b> ${downloads}</li>
+        </ul>
       </li>`
     )
     .join('');
 
-  gallery.insertAdjacentHTML('beforeend', markup);
-  lightbox.refresh(); 
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
 }
 
 export function clearGallery() {
-  gallery.innerHTML = '';
+  galleryEl.innerHTML = '';
 }
 
 export function showLoader() {
-  loader.classList.add('loader--visible');
-  loader.setAttribute('aria-hidden', 'false');
+  if (!loaderEl) return; 
+  loaderEl.classList.add('is-visible');
+  loaderEl.setAttribute('aria-hidden', 'false');
 }
 
 export function hideLoader() {
-  loader.classList.remove('loader--visible');
-  loader.setAttribute('aria-hidden', 'true');
-}
-
-
-function escapeHtml(str = '') {
-  return str
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#039;');
+  if (!loaderEl) return; 
+  loaderEl.classList.remove('is-visible');
+  loaderEl.setAttribute('aria-hidden', 'true');
 }
